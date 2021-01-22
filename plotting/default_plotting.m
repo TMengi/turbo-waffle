30d742fde2096fd13c9122ab806af20bbccdcdc1
Tyler McCown <tylermccown@engineering.ucla.edu>
2021-01-22 15:26:18 -0800

    added a script to restore default plotting options

---
+++ plotting/default_plotting.m
@@ -0,0 +1,10 @@
+function default_plotting()
+set(0, 'DefaultTextInterpreter', 'remove');
+set(0, 'DefaultLegendInterpreter', 'remove');
+set(groot, 'DefaultAxesTickLabelInterpreter', 'remove');
+set(0, 'defaultAxesFontSize', 'factory');
+set(0, 'defaultAxesLabelFontSize', 'factory');
+set(0, 'defaultLegendFontSize', 'factory');
+set(0, 'defaultLegendFontSizeMode', 'factory');
+set(0, 'defaultLineLineWidth', 'factory');
+end
--- plotting/pretty_pictures.m
+++ plotting/pretty_pictures.m
@@ -6,5 +6,5 @@ set(0, 'defaultAxesFontSize', 12);
 set(0, 'defaultAxesLabelFontSize', 1.3);
 set(0, 'defaultLegendFontSize', 14);
 set(0, 'defaultLegendFontSizeMode', 'manual');
-set(0, 'defaultLineLineWidth', 1);
+set(0, 'defaultLineLineWidth', 1.5);
 end
