c25d52c3f99ee462207b2c85e8a7072d03561f0f
Tyler McCown <tylermccown@engineering.ucla.edu>
2021-03-24 11:25:44 -0700

    added print statements if nargout is zero

--- controls_signals/step_stats.m
+++ controls_signals/step_stats.m
@@ -39,4 +39,33 @@ ts = t(find(abs(x - xss) > .01*(xss - x0), 1, 'last'));
 % steady-state error
 ess = ref - xss;
 
+% print stuff if not being stored as variables
+if nargout == 0
+    if tr < .01
+        fprintf('Rise time: %.3e\n', tr);
+    else
+        fprintf('Rise time: %.2f\n', tr);
+    end
+    
+    if tp < .01
+        fprintf('Peak time: %.3e\n', tp);
+    else
+        fprintf('Peak time: %.2f\n', tp);
+    end
+    
+    fprintf('Peak overshoot: %.1f%%\n', 100*Mp);
+    
+    if ts < .01
+        fprintf('Settling time: %.3e\n', ts);
+    else
+        fprintf('Settling time: %.2f\n', ts);
+    end
+    
+    if ess < .01
+        fprintf('Steady-state error: %.3e\n', ess);
+    else
+        fprintf('Steady-state error: %.2f\n', ess);
+    end
+end
+
 end
